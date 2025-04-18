a49d0037374dbf98ec198eca8670b794a5c51aac
Ulrich wandji <ulrichwandji2003@gmail.com>
2025-04-18 03:27:17 +0100

    oui

--- Code_barre/urls.py
+++ Code_barre/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("code_barre/<int:pk>/", code_barre, name="code_barre"),
-    # path('retour/', paiement_retour, name='paiement_retour'),
-    # path('notification/',   paiement_notification, name='paiement_notification'),
+    
 
 ]
